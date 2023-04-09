58158de535a49d4ed5e8b575af22810fd09db18e
vuductho96 <112962875+vuductho96@users.noreply.github.com>
2023-04-09 18:35:26 +0700

    TinhTongDuongCheoChinh

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Moi nhap so hang va so cot cua ma tran: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[,] matrix = new int[n, n];
+
+        for (int i = 0; i < n; i++) // nhập thong tin
+        {
+            for (int j = 0; j < n; j++)
+            {
+                Console.WriteLine("Nhap gia tri cua phan tu [{0}, {1}]:", i, j);
+                matrix[i, j] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+
+        double sum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            sum += matrix[i, i];
+        }
+
+        Console.WriteLine("Tong cac phan tu tren duong cheo chinh cua ma tran la: " + sum);
+        Console.ReadLine();
+    }
+}
